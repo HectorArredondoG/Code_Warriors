1a4768fe72a42c1d2e18540d0b7cae6ca9e05c8b
erick-malvaz <malvaezcamachoerick@gmail.com>
2024-10-02 13:23:49 -0600

    commit

---
+++ ninjas/src/App.svelte
@@ -0,0 +1,144 @@
+<script></script>
+<main>
+  <nav class="navbar navbar-expand-lg" id="navega">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#" style="color: white;">Navbar</a>
+    </div>
+  </nav>
+
+  <div id="quienes">
+    <div class="row">
+      <div class="col-6">
+        <h1>Quienes somos?</h1>
+        <p>Somos un pequeño grupo de desarrolladores webs, manejamos tecnologías de punta como Svelte, Boostrap, etc</p>
+      </div>
+      <div class="col-6">
+        
+        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div id="item-1">
+                <div class="row">
+                  <div class="col-4">
+                    <img src="https://www.shutterstock.com/image-photo/portrait-confident-handsome-mature-man-600nw-2292837691.jpg" alt="imagen" class="imagenes-presentacion">
+                  </div>
+                  <div class="col-8">
+                    <h4>Hector</h4>
+                    <hr>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat cupiditate, iure, pariatur assumenda officiis laborum praesentium id ducimus omnis placeat fuga? Qui nulla optio debitis alias voluptatum magni quis sed.</p>
+                    <a href=""></a><a href=""></a><a href=""></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div id="item-1">
+                <div class="row">
+                  <div class="col-4">
+                    <img src="https://www.shutterstock.com/image-photo/portrait-confident-handsome-mature-man-600nw-2292837691.jpg" alt="imagen" class="imagenes-presentacion">
+                  </div>
+                  <div class="col-8">
+                    <h4>Leonardo</h4>
+                    <hr>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat cupiditate, iure, pariatur assumenda officiis laborum praesentium id ducimus omnis placeat fuga? Qui nulla optio debitis alias voluptatum magni quis sed.</p>
+                    <a href=""></a><a href=""></a><a href=""></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div id="item-1">
+                <div class="row">
+                  <div class="col-4">
+                    <img src="https://www.shutterstock.com/image-photo/portrait-confident-handsome-mature-man-600nw-2292837691.jpg" alt="imagen" class="imagenes-presentacion">
+                  </div>
+                  <div class="col-8">
+                    <h4>Erick Malvaez</h4>
+                    <hr>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat cupiditate, iure, pariatur assumenda officiis laborum praesentium id ducimus omnis placeat fuga? Qui nulla optio debitis alias voluptatum magni quis sed.</p>
+                    <div class="redes">
+                      <a href="https://github.com/Erick-m4c4" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                      </svg></a>
+                      <a href="https://www.linkedin.com/in/malvaez-camacho-erick-103445326/" target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                          <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
+                        </svg>
+                      </a>
+                      <a href="https://wa.me/qr/DPUTDZ6WYKVJC1" target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+                          <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232"/>
+                        </svg>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="herramientas">
+    <div class="row">
+      <div class="col-5">
+        <div id="tecnologias_herramientas">
+          <div id="carouselExample" class="carousel slide" style="min-height: 100%; max-height: 100%; min-width:100%; max-height: 100%;">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Vitejs-logo.svg/1200px-Vitejs-logo.svg.png" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/800px-Bootstrap_logo.svg.png" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/800px-Svelte_Logo.svg.png" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="https://www.librebyte.net/wp-content/uploads/2021/05/MariaDB.png" class="d-block w-100" alt="...">
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="col-7">
+        <h1>Herramientas y Tecnologías Innovadoras</h1>
+        <hr>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita harum, sint omnis fuga sit suscipit repellat quia placeat, magni sed non et enim distinctio doloremque quibusdam dolorum alias molestiae ut.
+        Deleniti mollitia error amet laborum illum laboriosam corrupti illo modi, assumenda minima a dolorem tempore nobis molestias delectus pariatur esse alias similique at hic deserunt unde ullam. Praesentium, nesciunt perferendis?
+        Accusantium exercitationem nisi officia nostrum optio aut maxime cumque, eius ex harum reprehenderit, deserunt animi, odit similique iure vel dolorum velit voluptatem. Qui animi consectetur voluptas. Accusamus obcaecati recusandae nobis.
+        Libero quasi officiis adipisci modi! Vero sequi repellendus ipsa eveniet, ab veniam molestiae culpa vitae blanditiis dolorum, inventore, enim recusandae esse! Earum sunt in soluta magnam, iure ducimus et repudiandae!
+        At officiis impedit ex consectetur perferendis laudantium deserunt? Veniam harum architecto, libero vitae cum dolore accusantium asperiores tempora recusandae itaque mollitia laboriosam sunt impedit magni natus aspernatur quas nostrum fugiat.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div id="proyectos">
+    <h1>Galeria de proyectos: </h1>
+    
+  </div>
+
+  <footer>
+    <h1>Contactanos:</h1>
+
+  </footer>
+</main>
